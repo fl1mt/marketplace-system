6d0cb2d32bd027bac41ee77f4f3d2f518668ca2a
fl1mtDev <flmtdev@gmail.com>
2026-02-10 03:32:09 +0500

    feat: added delivery controller

---
+++ logistic-service/src/main/java/com/marketplace/logisticservice/delivery/DeliveryController.java
@@ -0,0 +1,23 @@
+package com.marketplace.logisticservice.delivery;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/delivery")
+public class DeliveryController {
+    private final DeliveryService deliveryService;
+
+    public DeliveryController(DeliveryService deliveryService) {
+        this.deliveryService = deliveryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DeliveryResponse>> getDeliveries(){
+        return ResponseEntity.ok(deliveryService.getDeliveries());
+    }
+}
--- logistic-service/src/main/java/com/marketplace/logisticservice/delivery/DeliveryService.java
+++ logistic-service/src/main/java/com/marketplace/logisticservice/delivery/DeliveryService.java
@@ -2,25 +2,29 @@ package com.marketplace.logisticservice.delivery;
 
 import com.marketplace.events.DeliveryCalculatedEvent;
 import com.marketplace.events.OrderCreatedEvent;
-import com.marketplace.logisticservice.delivery.Delivery;
-import com.marketplace.logisticservice.delivery.DeliveryRepository;
-import com.marketplace.logisticservice.delivery.DeliveryStatus;
 import com.marketplace.logisticservice.kafka.DeliveryCalculatedProducer;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class DeliveryService {
 
     private final DeliveryCalculatedProducer deliveryCalculatedProducer;
     private final DeliveryRepository deliveryRepository;
+    private final DeliveryMapper deliveryMapper;
 
-    public DeliveryService(DeliveryCalculatedProducer deliveryCalculatedProducer, DeliveryRepository deliveryRepository) {
+    public DeliveryService(DeliveryCalculatedProducer deliveryCalculatedProducer, DeliveryRepository deliveryRepository, DeliveryMapper deliveryMapper) {
         this.deliveryCalculatedProducer = deliveryCalculatedProducer;
         this.deliveryRepository = deliveryRepository;
+        this.deliveryMapper = deliveryMapper;
+    }
+
+    public List<DeliveryResponse> getDeliveries(){
+        return deliveryMapper.toResponseDtoList(deliveryRepository.findAll());
     }
 
     @Transactional
